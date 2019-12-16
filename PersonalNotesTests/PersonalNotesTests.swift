82e884eba3bb52e04f6febd328d9c9d7c783da9c
Lukas Mareda <lukas.mareda@novartis.com>
2019-12-16 08:37:10 +0100

    Initial Commit

---
+++ PersonalNotesTests/PersonalNotesTests.swift
@@ -0,0 +1,34 @@
+//
+//  PersonalNotesTests.swift
+//  PersonalNotesTests
+//
+//  Created by maredlu1 on 16/12/2019.
+//  Copyright © 2019 HomeWork. All rights reserved.
+//
+
+import XCTest
+@testable import PersonalNotes
+
+class PersonalNotesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
